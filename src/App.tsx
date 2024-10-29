4617ac5cb3008ae4476b1ea2cd1f05e9019a3ddc
Deepnarayan Sett <depnra1@gmail.com>
2024-10-29 23:39:22 +0530

    intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import './App.css'
-import Desktop from './components/Desktop'
-import Bar from './components/Task Bar/Bar'
-import { TabContextProvider } from './components/Task Bar/TabContext'
-import { FileSystemContextProvider } from './components/File System/FileSystemContext'
-function App() {
+import { useEffect, useState } from 'react';
+import './App.css';
+import Desktop from './components/Desktop';
+import Bar from './components/Task Bar/Bar';
+import { TabContextProvider } from './components/Task Bar/TabContext';
+import { FileSystemContextProvider } from './components/File System/FileSystemContext';
 
-  return (
-    <>
-      <div className='grain-bg'/>
-      <TabContextProvider>
-        <FileSystemContextProvider>
-          <Desktop/>
-          <Bar/>
-        </FileSystemContextProvider>
-      </TabContextProvider>
-    </>
-  )
+function App() {
+    const [splashScreenVisible, setSplashScreenVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setSplashScreenVisible(false);
+        }, 1800);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <>
+            {splashScreenVisible ? (
+                <div className="window">
+                    <div className="logo">
+                        <p className="top">Microsoft</p>
+                        <p className="mid">
+                            Windows<span>XP</span>
+                        </p>
+                        <p className="bottom">Professional</p>
+                    </div>
+                    <div className="loadcontainer">
+                        <div className="box"></div>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <div className="grain-bg" />
+                    <TabContextProvider>
+                        <FileSystemContextProvider>
+                            <Desktop />
+                            <Bar />
+                        </FileSystemContextProvider>
+                    </TabContextProvider>
+                </>
+            )}
+        </>
+    );
 }
 
-export default App
+export default App;
